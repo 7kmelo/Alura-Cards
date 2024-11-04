1e03961cd8c394491765cfc330d02e1005ebe4c0
7kmelo <00001103844180sp@al.educacao.sp.gov.br>
2024-11-04 11:51:43 -0300

    Kauã Melo

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é uma variável em programação?',
+    'É um espaço na memória para armazenar um valor que pode mudar.'
+)
+
+criaCartao(
+    'Programação',
+    'Para que serve um loop (ou laço) em programção?',
+    'Para repetir uma ação varias vezes automaticamente.'
+)
+
+criaCartao(
+    'Programação',
+    'Qual a diferença entre front-end e back-end?',
+    'Front-end é o que o usuário vê; back-end é o que acontece no servidor.'
+)
+
+criaCartao(
+    'Programação',
+    'O que significa "bug" em programação?',
+    'É um erro ou problema no código que impede o funcionamento correto.'
+)
